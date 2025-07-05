081c51e27d9534523917fd64c8cef2e3f596e986
RH <hurtsi@iki.fi>
2025-07-05 18:22:56 +0300

    Options record

--- kafka/producer/src/main/java/Producer.java
+++ kafka/producer/src/main/java/Producer.java
@@ -3,30 +3,32 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+record Options(int nReaders, int nWriters, int udpPort, int packetSize) {}
+
 public class Producer {
     private DatagramQueue queue;
     private Reader[] readers;
     private Writer[] writers;
+
     private static ExecutorService executorService;
     private static DatagramSocket readSocket;
 
-    private final int nReaders;
-    private final int nWriters;
-    private final int readPort;
+    private final Options opts;
 
     public Producer(String[] args) {
-        if  (args.length != 3) {
-            this.nReaders = 1;
-            this.nWriters = 1;
-            this.readPort = 6006;
+        if  (args.length != 4) {
+            this.opts = new Options(1, 1, 6006, 1024);
         } else {
-            this.nReaders = Integer.parseInt(args[0]);
-            this.nWriters = Integer.parseInt(args[1]);
-            this.readPort = Integer.parseInt(args[2]);
+            this.opts = new Options(
+                    Integer.parseInt(args[0]), // number of reader threads
+                    Integer.parseInt(args[1]), // number of writer threads
+                    Integer.parseInt(args[2]), // udp port for readers
+                    Integer.parseInt(args[3])  // datagram size
+            );
         }
 
-        executorService = Executors.newFixedThreadPool(nReaders + nWriters);
-        this.allocate(readPort);
+        executorService = Executors.newFixedThreadPool(opts.nReaders() + opts.nWriters());
+        this.allocate(opts.udpPort());
     }
 
     public static void main(String[] args) {
@@ -51,8 +53,8 @@ public class Producer {
 
     public void allocate(int readPort) {
         this.queue = new DatagramQueue(512);
-        this.readers = new Reader[nReaders];
-        this.writers = new Writer[nWriters];
+        this.readers = new Reader[opts.nReaders()];
+        this.writers = new Writer[opts.nWriters()];
 
         try {
             readSocket = new DatagramSocket(readPort);
@@ -61,11 +63,11 @@ public class Producer {
             Runtime.getRuntime().exit(1);
         }
 
-        for (int i = 0; i < nReaders; i++) {
-            this.readers[i] = new Reader(queue, readSocket, 1024);
+        for (int i = 0; i < opts.nReaders(); i++) {
+            this.readers[i] = new Reader(queue, readSocket, opts.packetSize());
         }
 
-        for (int i = 0; i < nWriters; i++) {
+        for (int i = 0; i < opts.nWriters(); i++) {
             this.writers[i] = new Writer(queue);
         }
     }
